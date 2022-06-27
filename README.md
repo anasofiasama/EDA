ee416752eb97598ac409e7ea68ecd015a2d1bbeb
Ana Sofía <anasofiasama@gmail.com>
2022-06-27 21:44:22 +0000

    EDA

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Template for Machine Learning projects
 
+La base de datos se obtiene del URL : https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv
+
+La transformación de los datos implica cambiar el tipo de dato de la siguiente manera:
+- 'name','host_name': string
+- 'neighbourhood_group','neighbourhood','room_type':'category
+- 'last_review':datetime with format='%Y/%m/%d'
+
+La limpieza de los datos implica:
+1. Eliminar los alojamientos cuyo precio por noche es igual a 0
+2. Se elimina aquellos alojamientos cuyo minimo de noches es mayor a 750
+
+> *Nota*: No se incluye el tratamiento del precio por noche del alojamiento en Manhattan en el 2013 que parecería ser anormal, ya que se necesitaría consultar a un especialista.
+
+
+
+
--- src/app.py
+++ src/app.py
@@ -1,4 +1,22 @@
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+# Importación de datos
+
+url = 'https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/' + \
+      'main/AB_NYC_2019.csv'
+df = pd.read_csv(url)
+
+# Transformación de datos
+
+df=df.astype({'name':'str','host_name':'str','neighbourhood_group':'category','neighbourhood':'category','room_type':'category'})
+df['last_review'] = pd.to_datetime(df['last_review'], format="%Y/%m/%d")
+
+# Limpieza de datos
+
+df=df[df['price']!=0] # Se eliminan los alojamientos cuyo precio por noche es igual a 0
+df=df[df['minimum_nights']<750] # Se elimina aquellos alojamientos cuyo minimo de noches es mayor a 750
+# No se incluye el tratamiento del precio por noche del alojamiento en Manhattan en el 2013 que parecería ser anormal, ya que se necesitaría consultar a un especialista.
+
+# Exportacion de la base procesada
+df.to_csv('/workspace/EDA/data/processed/Data.csv') 
