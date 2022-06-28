766d92554e5660afd44a5ad9ad91f20ec84d493c
Ana Sofía <anasofiasama@gmail.com>
2022-06-28 01:20:32 +0000

    EDA

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ La transformación de los datos implica cambiar el tipo de dato de la siguiente 
 - 'last_review':datetime with format='%Y/%m/%d'
 
 La limpieza de los datos implica:
-1. Eliminar los alojamientos cuyo precio por noche es igual a 0
+1. Se imputan los precios de los alojamientos cuyo precio por noche es igual a 0
 2. Se elimina aquellos alojamientos cuyo minimo de noches es mayor a 750
 
 > *Nota*: No se incluye el tratamiento del precio por noche del alojamiento en Manhattan en el 2013 que parecería ser anormal, ya que se necesitaría consultar a un especialista.
--- src/app.py
+++ src/app.py
@@ -14,7 +14,16 @@ df['last_review'] = pd.to_datetime(df['last_review'], format="%Y/%m/%d")
 
 # Limpieza de datos
 
-df=df[df['price']!=0] # Se eliminan los alojamientos cuyo precio por noche es igual a 0
+ # Se imputan los precios de los alojamientos cuyo precio por noche es igual a 0
+aux=df.groupby('neighbourhood_group')['price'].agg(['mean'])
+new_price=[]
+for i,r in df[['price']].iterrows():
+    if r['price']==0:
+        new_price.append(aux.loc[df['neighbourhood_group'][i],'mean']) # precio medio del area
+    else :
+        new_price.append(r['price'])
+df['price']=new_price
+
 df=df[df['minimum_nights']<750] # Se elimina aquellos alojamientos cuyo minimo de noches es mayor a 750
 # No se incluye el tratamiento del precio por noche del alojamiento en Manhattan en el 2013 que parecería ser anormal, ya que se necesitaría consultar a un especialista.
 
